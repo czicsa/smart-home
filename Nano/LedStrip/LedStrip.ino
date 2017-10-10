1e18bec6461c835fdfc75cc68ac820592376edf3
Czinke Csaba <czicsa@gmail.com>
2017-10-10 20:06:50 +0200

    Led strip driver extended with IR sensor capability

--- Nano/LedStrip/LedStrip.ino
+++ Nano/LedStrip/LedStrip.ino
@@ -10,15 +10,23 @@
 bool initialValueSent = false;
 
 #include <MySensors.h>
+#include <IRremote.h>
 
-#define FADE_SPEED 5     // make this higher to slow down
-#define LED_PIN 3         // pin number of the fet driver
-#define LED_ON 1          // GPIO value to write to turn on attached relay
-#define LED_OFF 0         // GPIO value to write to turn off attached relay
+#define FADE_SPEED 5    // make this higher to slow down
+#define IR_PIN 5        // input pin for IR sensor
+#define LED_PIN 5       // pin number of the fet driver
+#define LED_ON 1        // GPIO value to write to turn on attached relay
+#define LED_OFF 0       // GPIO value to write to turn off attached relay
+
+int actualValue; 				//actual value of the led strip output
+
+#define MAX_VALUE 255
+#define MIN_VALUE 0
 
 MyMessage msg(CHILD_ID_TEMP, V_STATUS);
+IRrecv irrecv(IR_PIN);
+decode_results signals;
 
-int actualValue;
 
 void before()
 {
@@ -37,6 +45,8 @@ void setup() {
   Serial.println("Requesting initial value from controller");
   request(CHILD_ID_TEMP, V_STATUS);
   wait(2000, C_SET, V_STATUS);
+    
+  irrecv.enableIRIn(); // enable input from IR receiver
 }
 
 void presentation()
@@ -50,14 +60,22 @@ void presentation()
  
  
 void loop() {
+    if (irrecv.decode(&signals)) {
+        Serial.println(signals.value, HEX);
+        if(signals.value == 0xFFA857){
+            fadeTo(actualValue + 50);
+        }
+        else if (signals.value == 0xFFE01F){
+            fadeTo(actualValue - 50);
+        }
+        irrecv.resume(); // get the next signal
+    }
 } 
 
 void receive(const MyMessage &message)
 {
     // We only expect one type of message from controller. But we better check anyway.
     if (message.type==V_STATUS) {
-         // Change relay state
-        message.getBool()?fadeTo(255):fadeTo(0);
         // Store state in eeprom
         saveState(message.sensor, message.getBool());
         // Write some debug info
@@ -65,13 +83,26 @@ void receive(const MyMessage &message)
         Serial.print(message.sensor);
         Serial.print(", New status: ");
         Serial.println(message.getBool());
-        send(msg.set(message.getBool()?LED_ON:LED_OFF));
+        
+        // Change relay state
+        fadeTo(message.getBool()?MAX_VALUE:MIN_VALUE);
     }
 }
 
-void fadeTo(int level) {
-  while(actualValue != level){
-      actualValue < level ? actualValue++ : actualValue--;
+void fadeTo(int fadeToValue) {
+  if(fadeToValue <= MIN_VALUE){
+    send(msg.set(LED_OFF));
+    fadeToValue = MIN_VALUE;
+  }
+  else if(fadeToValue > MAX_VALUE){
+    fadeToValue = MAX_VALUE;
+  }
+  else {
+    send(msg.set(LED_ON));
+  }
+  
+  while(actualValue != fadeToValue){
+      actualValue < fadeToValue ? actualValue++ : actualValue--;
       analogWrite(LED_PIN, actualValue);
       delay(FADE_SPEED);
   }
